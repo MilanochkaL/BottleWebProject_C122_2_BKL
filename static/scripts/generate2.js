9dedfb6436637dec1d8c14b2e37e00b01b593245
Nadya <kuznechik25092005@gmail.com>
2024-05-18 20:13:45 +0300

    добавила комменатрии к кодам

--- static/scripts/generate2.js
+++ static/scripts/generate2.js
@@ -1,46 +1,46 @@
 function addInputs() {
-    let numberOfInputs = parseInt(document.getElementById('inputCount').value);
-    if (isNaN(numberOfInputs) || numberOfInputs <= 0 || numberOfInputs > 15) {
-        alert('Введите корректное число от 1 до 15. В противном случае что-то может пойти не так!');
-        return;
+    let numberOfInputs = parseInt(document.getElementById('inputCount').value); // Получаем значение из input с id 'inputCount' и преобразуем его в число
+    if (isNaN(numberOfInputs) || numberOfInputs <= 0 || numberOfInputs > 15) { // Проверяем, является ли число корректным (от 1 до 15)
+        alert('Введите корректное число от 1 до 15. В противном случае что-то может пойти не так!'); // Выводим сообщение об ошибке, если число некорректно
+        return; // Завершаем функцию
     }
 
-    let container = document.getElementById('inputContainer'); // Это место для новых полей
-    while (container.hasChildNodes()) {
-        container.removeChild(container.lastChild); // Освобождаем место от старых элементов
+    let container = document.getElementById('inputContainer'); // Получаем элемент с id 'inputContainer', где будут добавляться новые поля
+    while (container.hasChildNodes()) { // Удаляем все дочерние элементы из контейнера
+        container.removeChild(container.lastChild); // Удаляем последний дочерний элемент
     }
 
-    let divNames = [];
-    for (let j = 0; j < numberOfInputs; j++) {
-        let divName = `div${j}`;
-        divNames.push(divName);
+    let divNames = []; // Создаем массив для хранения имен новых div-элементов
+    for (let j = 0; j < numberOfInputs; j++) { // Цикл для создания новых div-элементов
+        let divName = `div${j}`; // Генерируем имя для каждого div-элемента
+        divNames.push(divName); // Добавляем имя в массив
 
-        let div = document.createElement('div');
-        div.id = divName;
-        div.style = "display: flex";
-        container.appendChild(div); // Добавляем новое поле в контейнер
+        let div = document.createElement('div'); // Создаем новый div-элемент
+        div.id = divName; // Устанавливаем ему id
+        div.style = "display: flex"; // Устанавливаем стиль для отображения в виде флекса
+        container.appendChild(div); // Добавляем div-элемент в контейнер
 
-        let container2 = document.getElementById(divName); // Это место для новых полей
-        for (let i = 0; i < numberOfInputs; i++) {
-            let input = document.createElement('input');
-            if (i === j) {
-                input.value = "0";
-                input.setAttribute('readonly', '');
-                input.style = "width: 30px; margin: 3px; border-radius: 10px; border-color: #a8b1e5; border-width: 2px; border-style: solid;";
-            } else {
-                input.value = "0";
-                input.style = "width: 30px; margin: 3px; border-radius: 10px; border-color: #a8b1e5; border-width: 2px; border-style: solid;";
-                input.oninput = function () {
-                    let input2 = document.getElementById(`dynamicInput${i}${j}`); // Это место для новых полей
-                    input2.value = input.value;
+        let container2 = document.getElementById(divName); // Получаем созданный div-элемент
+        for (let i = 0; i < numberOfInputs; i++) { // Цикл для создания input-элементов внутри каждого div
+            let input = document.createElement('input'); // Создаем новый input-элемент
+            if (i === j) { // Если индексы совпадают, устанавливаем input как readonly
+                input.value = "0"; // Устанавливаем значение по умолчанию
+                input.setAttribute('readonly', ''); // Делаем input нередактируемым
+                input.style = "width: 30px; margin: 3px; border-radius: 10px; border-color: #a8b1e5; border-width: 2px; border-style: solid;"; // Устанавливаем стиль
+            } else { // Для остальных input
+                input.value = "0"; // Устанавливаем значение по умолчанию
+                input.style = "width: 30px; margin: 3px; border-radius: 10px; border-color: #a8b1e5; border-width: 2px; border-style: solid;"; // Устанавливаем стиль
+                input.oninput = function () { // Обработчик события ввода данных
+                    let input2 = document.getElementById(`dynamicInput${i}${j}`); // Получаем другой input по его id
+                    input2.value = input.value; // Присваиваем ему значение текущего input
                 };
             }
-            input.name = `dynamicInput${j}${i}`; // Имена для каждого поля для избежания ошибок
-            input.id = `dynamicInput${j}${i}`; // Имена для каждого поля для избежания ошибок
-            input.value = "0";
-            container2.appendChild(input); // Добавляем новое поле в контейнер
-            container2.appendChild(document.createElement('br')); // Предусматриваем дополнительное пространство между полями
+            input.name = `dynamicInput${j}${i}`; // Устанавливаем имя для input
+            input.id = `dynamicInput${j}${i}`; // Устанавливаем id для input
+            input.value = "0"; // Устанавливаем значение по умолчанию
+            container2.appendChild(input); // Добавляем input в div
+
+            container2.appendChild(document.createElement('br')); // Добавляем элемент 'br' для создания дополнительного пространства между полями
         }
     }
-
 }
--- static/scripts/graph_constr.js
+++ static/scripts/graph_constr.js
@@ -1,45 +1,45 @@
 function addGraph1() {
-    let container = document.getElementById('graph_div1');
-    while (container.hasChildNodes()) {
-        container.removeChild(container.lastChild);
+    let container = document.getElementById('graph_div1'); // Получаем элемент с id 'graph_div1'
+    while (container.hasChildNodes()) { // Пока в контейнере есть дочерние элементы
+        container.removeChild(container.lastChild); // Удаляем последний дочерний элемент
     }
-    let div = document.createElement('div');
-    div.id = "mynetwork_1";
-    div.className = "mynetwork_1";
-    container.appendChild(div);
+    let div = document.createElement('div'); // Создаем новый div
+    div.id = "mynetwork_1"; // Устанавливаем id для div
+    div.className = "mynetwork_1"; // Устанавливаем класс для div
+    container.appendChild(div); // Добавляем div в контейнер
 
-    let inputContainer = document.getElementById('inputContainer');
-    let matrixRows = inputContainer.children;
-    let nodes = [];
-    let edges = [];
+    let inputContainer = document.getElementById('inputContainer'); // Получаем элемент с id 'inputContainer'
+    let matrixRows = inputContainer.children; // Получаем дочерние элементы контейнера
+    let nodes = []; // Инициализируем массив для узлов
+    let edges = []; // Инициализируем массив для ребер
 
-    // Create nodes
+    // Создание вершин
     for (let i = 0; i < matrixRows.length; i++) {
-        nodes.push({ id: i + 1, label: `${i + 1}` });
+        nodes.push({ id: i + 1, label: `${i + 1}` }); // Добавляем узел с id и меткой (номер узла)
     }
 
-    // Create edges
+    // Создание ребер
     for (let i = 0; i < matrixRows.length; i++) {
-        let rowInputs = matrixRows[i].children;
+        let rowInputs = matrixRows[i].children; // Получаем дочерние элементы текущей строки матрицы
         for (let j = 0; j < rowInputs.length; j++) {
-            let inputValue = rowInputs[j].value;
-            if (inputValue === '1' && i !== j) {
+            let inputValue = rowInputs[j].value; // Получаем значение текущего input
+            if (inputValue === '1' && i !== j) { // Если значение равно '1' и это не диагональный элемент
                 // Проверяем, что такого ребра еще нет в массиве edges
                 if (!edges.some(edge => (edge.from === i + 1 && edge.to === j / 2 + 1) || (edge.from === j / 2 + 1 && edge.to === i + 1))) {
-                    edges.push({ from: i + 1, to: j / 2 + 1 });
+                    edges.push({ from: i + 1, to: j / 2 + 1 }); // Добавляем ребро в массив edges
                 }
             }
         }
     }
 
-    // Network options
+    // Опции для сети
     var options = {};
 
-    // Create network data and display the graph
-    var networkContainer = document.getElementById('mynetwork_1');
+    // Создание данных сети и отображение графа
+    var networkContainer = document.getElementById('mynetwork_1'); // Получаем элемент для отображения графа
     var data = {
-        nodes: nodes,
-        edges: edges
+        nodes: nodes, // Узлы
+        edges: edges // Ребра
     };
-    var network = new vis.Network(networkContainer, data, options);
+    var network = new vis.Network(networkContainer, data, options); // Создаем и отображаем граф
 }
